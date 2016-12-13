2d3d5a9b4e5a1446a121b8ea1a27c8d76cf0c942
Paula <paula.mecatronica@gmail.com>
2016-12-12 23:58:24 -0200

    add espresso class

---
+++ ViewTypesRecyclerView/app/src/androidTest/java/net/paulacr/viewtypesrecyclerview/MainActivityTest.java
@@ -0,0 +1,20 @@
+package net.paulacr.viewtypesrecyclerview;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void hasAllLists() {
+
+    }
+}
